4f18c28e8d8261eb7ae355bda0149c3230be3bab
Dhruv-Adlakha <dhruvadl25@gmail.com>
2021-02-08 13:08:06 +0530

    Theme and landing page ui

--- client/src/components/Layout/LandingPage.js
+++ client/src/components/Layout/LandingPage.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import Navbar from '../Layout/Navbar';
+import landingPageImage from '../../images/landingPage.jpg';
 
 const LandingPage = () => {
   return (
     <div>
-      <Navbar />
+      <div className='landingPage__section'>
+        <img src={landingPageImage} alt='' />
+        <div className='landingPage__section__content'>
+          <h2>Sarthi</h2>
+          <ul>
+            <li>Sarthi is a medical help application.</li>
+            <li>
+              If you need medical help and cannot afford the cost we will help
+              you.
+            </li>
+            <li>
+              People can volunteer for hospital visits, medicine delivery and
+              donations
+            </li>
+            <li>
+              Doctors can help by providing free checkup facility for the needy
+            </li>
+          </ul>
+          <div className='landingPage__section__content__buttons'>
+            <button>Login</button>
+            <button>Sign up</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
