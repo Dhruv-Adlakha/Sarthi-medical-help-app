ed61f10f6c1e5482a7c8120e56d00624538d4a82
Dhruv-Adlakha <dhruvadl25@gmail.com>
2021-02-14 16:27:48 +0530

    Add Needy users backend routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const entryRouter = require('./routes/entry');
 const adminRouter = require('./routes/admin');
+const needyRouter = require('./routes/needy');
 require('./db/mongoose');
 
 const express = require('express');
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(entryRouter);
 app.use(adminRouter);
+app.use(needyRouter);
 
 const port = process.env.PORT || 5000;
 
--- src/routes/needy.js
+++ src/routes/needy.js
@@ -1,5 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const Needy = require('../models/Needy');
+const Request = require('../models/Request');
+const Doctor = require('../models/Doctor');
+const mongoose = require('mongoose');
+
+//get my profile
+router.get('/needy/profiles/:id', async (req, res) => {
+  try {
+    const user = await Needy.findById(req.params.id);
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//update my profile
+router.patch('/needy/updateProfile/:id', async (req, res) => {
+  try {
+    const user = await Needy.findById(req.params.id);
+    const updates = Object.keys(req.body);
+    if (!user) {
+      return res.status(404).send('User is not present');
+    }
+    updates.map((update) => {
+      user[update] = req.body[update];
+    });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//make a request
+router.post('/needy/helpRequest', async (req, res) => {
+  try {
+    const patient = await Needy.findById(req.body.patientId);
+    const doctor = await Doctor.findById(req.body.doctorId);
+    const helpRequest = new Request({
+      problem: req.body.problem,
+      description: req.body.description,
+      patient: patient._id,
+      doctor: doctor._id,
+    });
+    await helpRequest.save();
+    res.send(helpRequest);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
